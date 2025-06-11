fa191a4666873bb279781069762c52dd014343ff
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2025-06-11 14:53:13 -0700

    u[

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2023 The OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device.mk
-PRODUCT_MAKEFILES := device/xiaomi/water/device.mk
+    $(LOCAL_DIR)/twrp_water.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_water-eng
+
